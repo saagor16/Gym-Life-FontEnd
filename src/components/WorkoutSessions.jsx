493808f27cbdf8de797944324022c302a8b60c96
saagor16 <saagor.16@gmail.com>
2024-11-30 18:57:10 +0600

    Replaced placeholder text with new content in the workout session section

--- src/components/WorkoutSessions.jsx
+++ src/components/WorkoutSessions.jsx
@@ -4,51 +4,45 @@ const WorkoutSessions = () => {
       <div className="wrapper">
         <h1>TOP WORKOUT SESSION</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
-          quisquam. Eaque alias, blanditiis quidem maiores illo unde ipsam!
+          Push your limits with our expertly designed workout sessions at
+          Gym-Life. Get stronger, fitter, and healthier.
         </p>
         <img src="/img5.jpg" alt="workout" />
       </div>
       <div className="wrapper">
         <h1>FEATURED BOOTCAMPS</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-          impedit quasi sunt amet rerum accusamus odio eveniet unde?
+        Join Our Most Popular Bootcamp Sessions and Achieve Your Fitness Goals with Gym-Life.
         </p>
         <div className="bootcamps">
           <div>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <h4>Core & Abs Bootcamp</h4>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              saepe repellendus nemo sit facere ipsam!
+            Strengthen your core and sculpt your abs with targeted exercises. This bootcamp focuses on stability, posture, and toning the midsection to achieve a defined and strong core.
             </p>
           </div>
           <div>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <h4>Cardio Burn Bootcamp</h4>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              saepe repellendus nemo sit facere ipsam!
+            Boost your cardiovascular health and endurance while burning fat. This bootcamp includes running, cycling, jump rope, and other cardio exercises to help you shed pounds and build stamina.
             </p>
           </div>
           <div>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <h4> Yoga & Flexibility Bootcamp</h4>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              saepe repellendus nemo sit facere ipsam!
+            Improve flexibility, balance, and mental focus through yoga and stretching. Ideal for reducing stress, enhancing mobility, and boosting overall well-being.
             </p>
           </div>
           <div>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <h4>Bootcamp for Beginners</h4>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              saepe repellendus nemo sit facere ipsam!
+            If you are new to fitness or returning after a break, this beginner-friendly bootcamp offers low-impact exercises and gradual progression.
             </p>
           </div>
           <div>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <h4>Advanced Athlete Bootcamp</h4>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              saepe repellendus nemo sit facere ipsam!
+            Designed for seasoned athletes or those looking to push their limits, this bootcamp features challenging exercises to enhance performance, agility, and explosive power.
             </p>
           </div>
         </div>
