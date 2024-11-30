9ccdebac934e4f842c1dd1f892fbf583e1e3199e
saagor16 <saagor.16@gmail.com>
2024-11-30 18:39:49 +0600

    Completed full implementation of the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,23 @@
-
 const Footer = () => {
     return (
+      <footer>
         <div>
-            
+          <p>© 2024 Gym Life. All Rights Reserved.</p>
+          <p>
+            Developed by{" "}
+            <a
+              href="https://github.com/saagor16"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "none" }}
+            >
+              <span style={{ color: "#0095ff", fontWeight: "bold" }}>Sagor</span>
+            </a>
+          </p>
         </div>
+      </footer>
     );
-};
-
-export default Footer;
+  };
+  
+  export default Footer;
+  
