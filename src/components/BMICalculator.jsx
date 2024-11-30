12394f652f298ac7f5a783a98d75c2991a5bb84d
saagor16 <saagor.16@gmail.com>
2024-11-30 11:25:30 +0600

    project er componenents folder create and file create

---
+++ src/components/BMICalculator.jsx
@@ -0,0 +1,10 @@
+
+const BMICalculator = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BMICalculator;
---
+++ src/components/WorkoutSessions.jsx
@@ -0,0 +1,10 @@
+
+const WorkoutSessions = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default WorkoutSessions;
